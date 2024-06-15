cac21ade5227ab0f88285f495cb6ed81ca0af38a
Chris Johnston <chris@raaum.us>
2024-06-15 15:33:30 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import logo from './img/logo.svg';
+import ticket from './img/golden_ticket.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ticket} className="App-logo" alt="logo" />
+        <p/>
+        <p/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SCOR3-TIX
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
